3b6a1b810cb88838caae1b49b5caf0f57fce74c1
MylesWritesCode <myles@themapletree.io>
2022-06-28 07:39:03 -1000

    adds example app back

--- apps/client/app/components/SnakeGame.client.tsx
+++ apps/client/app/components/SnakeGame.client.tsx
@@ -1,9 +1,13 @@
+import init from 'snake-game';
+
 export function SnakeGame() {
+  const snake = init;
+
   return (
     <div id="snakeGameContainer">
       <div>
         <h1>snake game</h1>
       </div>
     </div>
-  )
+  );
 }
