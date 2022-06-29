70c2275c74c78e35e4d5d02d0b1d02a3a085362e
MylesWritesCode <myles@themapletree.io>
2022-06-28 14:52:34 -1000

    re-init next

--- apps/client-next/pages/index.tsx
+++ apps/client-next/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Button } from "ui";
+import type { NextPage } from 'next'
 
-export default function Web() {
+const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-      <script type="module"></script>
+    <div className="flex items-center justify-center min-h-screen p-2 min-w-screen bg-gradient-to-br from-emerald-400 to-indigo-300">
+      <div className="bg-slate-50 w-[500px] p-2 h-[500px]">
+        <h1>works</h1>
+      </div>
     </div>
-  );
+  )
 }
+
+export default Home
